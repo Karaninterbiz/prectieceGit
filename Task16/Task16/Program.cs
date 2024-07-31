38404455975fcb2e68dc94d36980222592ea053e
Arpit8Interbiz <arpit.swarnkar@interbizconsulting.com>
2024-07-31 17:09:24 +0530

    Add files via upload

---
+++ Task16/Task16/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task16
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] input = Console.ReadLine().Split(',');
+                int X = int.Parse(input[0]);
+                int Y = int.Parse(input[1]);
+                int Z = int.Parse(input[2]);
+
+                if (Math.Pow(X + Y, 2) == Math.Pow(Z, 2))
+                {
+                    Console.WriteLine("Test Case Passed");
+                }
+                else
+                {
+                    Console.WriteLine("Test Case Failed");
+                }
+            }
+        }
+    }
+}
