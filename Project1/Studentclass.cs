2229dcf5f33f954b4faf09c20d5df97cab66bbc1
Pooja040998 <pooja.sahu@interbizconsulting.com>
2024-08-24 11:50:05 +0530

    Add files via upload

---
+++ Project1/Studentclass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StudentManaGE
+{
+    public class Students
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int RollNumber { get; set; }
+        public List<string> Hobbies { get; set; } 
+        public int SubMaths { get; set; }
+        public ClassNumber Class { get; set; }
+        public int SubEnglish { get; set; }
+        public int SubHindi { get; set; }
+        public string Address { get; set; }
+    }
+}
